1179cad894d19a7d23cfb18087d3e19ba72bc56e
Pamela Frieze <pfrieze@gmail.com>
2018-08-06 21:53:45 +0000

    add validation requirements

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,7 @@
 class Patient < ActiveRecord::Base
   belongs_to :doctor
+
+  validates :username, :presence => true, :uniqueness => true
+  validates :password, :presence => true, :length { minimum: 5 }
+
 end
