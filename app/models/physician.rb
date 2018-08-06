65f0ef260267148442073271912c5298c4237948
Pamela Frieze <pfrieze@gmail.com>
2018-08-06 21:47:14 +0000

    model buildouts

---
+++ app/models/physician.rb
@@ -0,0 +1,9 @@
+class Physician < ActiveRecord::Base
+  has_secure_password
+  has_many :patients
+
+  validates :username, :presence => true, :uniqueness => true
+  validates :npi, :presence => true, :uniqueness => true
+  validates :password, :presence => true
+
+end
