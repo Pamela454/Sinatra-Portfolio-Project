38178393c4c12f53144f0ae476665d47e8ff81ff
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-03-23 13:08:46 -0400

    README usage update

--- app/controllers/patientscontroller.rb
+++ app/controllers/patientscontroller.rb
@@ -2,74 +2,74 @@ require 'sinatra/base'
 require 'rack-flash'
 
 class PatientsController < ApplicationController
-    use Rack::Flash
+  use Rack::Flash
 
-    get '/patients/login' do
-        if self.current_user.class == Patient
-          @patient = Patient.find_by(id: session[:id])
-          session[:id] = @patient.id
-          erb :"/patients/show"
-        else
-          erb :"/patients/login"
-        end
+  get '/patients/login' do
+    if self.current_user.class == Patient
+      @patient = Patient.find_by(id: session[:id])
+      session[:id] = @patient.id
+      erb :"/patients/show"
+    else
+      erb :"/patients/login"
     end
+  end
 
-    post '/patients/login' do
-      @patient = Patient.find_by(username: params[:username])
-      #log_in 
-      current_user
-      if @patient && @patient.authenticate(params[:password]) #check to see if password matches stored password
-            session[:username] = params[:username]
-            session[:id] = @patient.id
-            session[:user_type] = "patient"
-            redirect "/patients/#{@patient.id}"
+  post '/patients/login' do
+    @patient = Patient.find_by(username: params[:username])
+    #log_in 
+    current_user
+    if @patient && @patient.authenticate(params[:password]) #check to see if password matches stored password
+      session[:username] = params[:username]
+      session[:id] = @patient.id
+      session[:user_type] = "patient"
+      redirect "/patients/#{@patient.id}"
+    else
+      flash[:message] = "Could not authenticate login."
+      redirect '/patients/login'
+    end
+  end
+
+  get '/patients/new' do #protect from non physicians editing
+    if self.current_user.class.name == "Physician"
+      erb :"/patients/new"
+    else
+      flash[:message] = "You do not have access to that feature."
+      redirect '/'
+    end
+  end
+
+
+  post '/patients/new' do
+    if params[:username] == nil && params[:password] == nil
+      flash[:message] = "Please enter both username and password to create a new patient."
+      redirect "/patients/new"
+    else
+      params[:username] && params[:password] != nil
+      @patient = Patient.create(username: params[:username], password: params[:password], physician_id: session[:id],
+      medical_history: params[:medical_history], active_problems: params[:active_problems])
+      if @patient.save
+        @physician = Physician.find_by(id: session[:id])
+        @patients = @physician.patients
+        flash[:message] = "Successfully added new patient."
+        erb :"/physicians/show"
       else
-            flash[:message] = "Could not authenticate login."
-            redirect '/patients/login'
-      end
-    end
-
-    get '/patients/new' do #protect from non physicians editing
-      if self.current_user.class.name == "Physician"
-        erb :"/patients/new"
-      else
-        flash[:message] = "You do not have access to that feature."
-        redirect '/'
-      end
-    end
-
-
-    post '/patients/new' do
-      if params[:username] == nil && params[:password] == nil
-        flash[:message] = "Please enter both username and password to create a new patient."
+        flash[:message] = "Not valid profile data."
         redirect "/patients/new"
-      else
-        params[:username] && params[:password] != nil
-        @patient = Patient.create(username: params[:username], password: params[:password], physician_id: session[:id],
-         medical_history: params[:medical_history], active_problems: params[:active_problems])
-         if @patient.save
-           @physician = Physician.find_by(id: session[:id])
-           @patients = @physician.patients
-           flash[:message] = "Successfully added new patient."
-           erb :"/physicians/show"
-         else
-           flash[:message] = "Not valid profile data."
-           redirect "/patients/new"
-         end
-     end
-   end
+      end
+    end
+  end
 
 
   get '/patients/:id' do  #creating a route variable. should always be after patients/new route
     #current user is nil 
-      if session[:user_type] == "patient" && session[:id] == params[:id].to_i#only viewed by patients
-        @patient = Patient.find_by(id: session[:id])
-        flash[:message] = "Successful login."
-        erb :"/patients/show"
-      else
-        flash[:message] = "You do not have access to that feature."
-        erb :"/patients/login"
-      end
+    if session[:user_type] == "patient" && session[:id] == params[:id].to_i#only viewed by patients
+      @patient = Patient.find_by(id: session[:id])
+      flash[:message] = "Successful login."
+      erb :"/patients/show"
+    else
+      flash[:message] = "You do not have access to that feature."
+      erb :"/patients/login"
+    end
   end
 
   get '/patients/:id/edit' do #can only be edited by a physician
@@ -91,8 +91,8 @@ class PatientsController < ApplicationController
     if params[:active_problems] != ""
       @patient.update_attribute(:active_problems, params[:active_problems])
     end
-      flash[:message] = "Successfully edited patient profile."
-      redirect "/physicians/#{@physician.id}"
+    flash[:message] = "Successfully edited patient profile."
+    redirect "/physicians/#{@physician.id}"
     #else
     #  flash[:message] = "Information supplied does not meet requirements. Please try again."
     #  redirect "/patients/#{@patient.id}/edit"
@@ -100,23 +100,26 @@ class PatientsController < ApplicationController
   end
 
   delete "/patients/:id/delete" do
+    binding.pry
     @patient = Patient.find_by(id: params[:id])
-    @physician = Physician.find_by(id: @session[:id])
+    @physician = Physician.find_by(id: params[:id])
 
     if @current_user == nil
       flash[:message] = "You do not have access to that feature."
       redirect "/"
+      binding.pry 
     elsif @current_user == Patient.find_by(username: @session[:username])
       flash[:message] = "You are not permitted to delete this patient."
       redirect "/physicians/#{@physician.id}"
     else
+      binding.pry 
       @patient.delete
       redirect "/physicians/#{@physician.id}"
     end
   end
 
   get '/logout' do
-      session.clear
-      redirect "/"
-    end
+    session.clear
+    redirect "/"
   end
+end
