9606d7f144add5f0fbcfb9d70c916cc7ed8f18eb
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-04-05 13:00:21 -0400

    environment file update

--- app/controllers/applicationcontroller.rb
+++ app/controllers/applicationcontroller.rb
@@ -6,13 +6,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public' 
     set :views, 'app/views' 
-    enable :sessions   
+    enable :sessions 
     set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   end 
 
   get "/" do
-    erb :index
+    binding.pry 
     @session = session #session hash created. views have access to hash. 
+    erb :index
   end
 
   helpers do
@@ -22,9 +23,13 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user 
-      @current_user ||= session[:user_type] == "patient" ? #current_user or session user == patient?
-      Patient.find_by(username: session[:username]) :
-      Physician.find_by(username: session[:username])
+      if session[:user_type] == nil
+        session[:user_type] == "none"
+      else
+        @current_user ||= session[:user_type] == "patient" ? #current_user or session user == patient?
+        Patient.find_by(username: session[:username]) :
+        Physician.find_by(username: session[:username])
+      end
     end
   end
 end
