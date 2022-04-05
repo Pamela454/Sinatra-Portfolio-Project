a9743c9546a0e04016e4516f70a2004330f01f87
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-04-05 13:00:21 -0400

    environment file update

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 ENV['SINATRA_ENV'] ||= "development"
-ENV['SESSION_SECRET'] ||= "superdupersecret" 
+ENV['SESSION_SECRET'] ||= "44b3f19a25628c5f2134b3cec3e758f3d0db4caaaa9a7c5af9fa1edf49ff1e3e7c84157aaa090c6e49ed36d3a10925fbab418fbc2dedf3518895835519840af0" 
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
