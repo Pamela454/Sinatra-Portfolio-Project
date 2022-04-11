56892409f6d175585a52b9d75c7c0e84d0e7f90d
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-04-11 16:04:32 -0400

    correct new_user spacing in create_physician.erb file

--- app/controllers/physicianscontroller.rb
+++ app/controllers/physicianscontroller.rb
@@ -10,6 +10,7 @@ class PhysiciansController < ApplicationController
       redirect '/patients/:id'
     else
       @new_user = Physician.create(username: params[:username], npi: params[:npi], password: params[:password])
+            binding.pry 
       erb :"/physicians/create_physician"
     end
   end
