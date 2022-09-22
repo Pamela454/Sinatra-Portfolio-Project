aa9c3cb6cd8decc8f2362cdcb4146ebc0a1470ab
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-09-22 12:58:14 -0400

    postgres url change

--- config/environment.rb
+++ config/environment.rb
@@ -2,28 +2,23 @@
 require 'bundler/setup'
 
 configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-ActiveRecord::Base.establish_connection(
+  ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+  db = URI.parse(ENV['postgres://kkppklmjzefocx:8a1c6beba56e7e1c'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 end
 
 require_all 'app'
