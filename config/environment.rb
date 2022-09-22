741462db2c11835abcab494b2b57ee294fefa1c7
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-09-22 13:19:05 -0400

    config file update

--- config/environment.rb
+++ config/environment.rb
@@ -1,23 +1,11 @@
-#require 'dotenv'
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
 require 'dotenv'
 Dotenv.load
 
-configure :development do
-  set :database_file, './database.yml'
-end
-
-configure :production do
-  db = URI.parse(ENV['postgres://kkppklmjzefocx:8a1c6beba56e7e1c'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
+set :database_file, './database.yml'
 
 require_all 'app'
