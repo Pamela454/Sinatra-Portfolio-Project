8e0609965d07d1125133a1b6ae869e5abc164668
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-09-24 09:18:56 -0400

    add id to migration file

--- db/migrate/06_change_integer_limit_patients.rb
+++ db/migrate/06_change_integer_limit_patients.rb
@@ -1,5 +1,6 @@
 class ChangeIntegerLimitPatients < ActiveRecord::Migration[5.2]
     def change
       change_column :patients, :physician_id, :integer, limit: 8
+      change_column :patients, :id, :integer, limit: 8 
     end 
   end
