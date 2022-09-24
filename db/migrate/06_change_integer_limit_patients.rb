9bf7898a0d545addef9f16e2c95929a9a0d27602
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-09-24 09:12:14 -0400

    update migration files

--- db/migrate/06_change_integer_limit_patients.rb
+++ db/migrate/06_change_integer_limit_patients.rb
@@ -1,5 +1,5 @@
 class ChangeIntegerLimitPatients < ActiveRecord::Migration[5.2]
     def change
-      change_column :patients, :patient_id, :integer, limit: 8
+      change_column :patients, :physician_id, :integer, limit: 8
     end 
   end
